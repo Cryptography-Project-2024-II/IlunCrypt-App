f5784d8bf9913fa0136956de1ffd6c3b7a68b360
edgarbg82 <ebabativag@unal.edu.co>
2025-02-23 22:30:10 -0500

    Added implementation of classic encryption and decryption models

--- src/main/java/com/iluncrypt/iluncryptapp/models/algorithms/classic/HillCipher.java
+++ src/main/java/com/iluncrypt/iluncryptapp/models/algorithms/classic/HillCipher.java
@@ -1,4 +1,12 @@
 package com.iluncrypt.iluncryptapp.models.algorithms.classic;
 
+import com.iluncrypt.iluncryptapp.models.Alphabet;
+import com.iluncrypt.iluncryptapp.models.Cryptosystem;
+import com.iluncrypt.iluncryptapp.models.enums.CaseHandling;
+import com.iluncrypt.iluncryptapp.models.enums.UnknownCharHandling;
+import com.iluncrypt.iluncryptapp.models.enums.WhitespaceHandling;
+import com.iluncrypt.iluncryptapp.models.keys.AffineKey;
+import com.iluncrypt.iluncryptapp.models.keys.Key;
+
 public class HillCipher {
 }
